12d53b0904784ee9a5c8e4fdcaec472942ab8069
Hồ Thị Hương <huong.ho24@student.passerellesnumeriques.org>
2023-12-20 14:59:43 +0700

    register

--- FrontEnd/App.tsx
+++ FrontEnd/App.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
-
-import {StyleSheet, Text, View} from 'react-native';
+import Open from './android/app/src/Screens/Registers/Open';
+import {View} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <View>
-      <Text>Hello</Text>
+     <Open/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
